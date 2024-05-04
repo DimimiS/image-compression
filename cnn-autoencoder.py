5a25915f7f675571701eb853ed30e2fea4b03f5f
DimimiS <dimiskama115@gmail.com>
2024-05-04 08:45:54 +0200

    cleaned up code

--- cnn-autoencoder.py
+++ cnn-autoencoder.py
@@ -50,39 +50,3 @@ model.fit(x_train, x_train, epochs=50, batch_size=32, shuffle=True, validation_d
 
 # Save model
 model.save('autoencoder.keras')
-
-# Predict the test images
-decoded_imgs = model.predict(x_test)
-
-# # Extract the code layer
-# code_layer = Model(inputs=model.input, outputs=model.get_layer('Code').output)
-# code = code_layer.predict(x_test)
-# code = code.reshape(32, 32, 3)
-
-# Display the first 10 images from the test set
-plt.figure(figsize=(20, 2))
-for i in range(10):
-    ax = plt.subplot(1, 10, i + 1)
-    plt.imshow(x_test[i])
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-# # Display the first 10 compressed images
-# plt.figure(figsize=(10, 1))
-# for i in range(10):
-#     ax = plt.subplot(1, 10, i + 1)
-#     plt.imshow(code[i])
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-# plt.show()
-
-# Display the first 10 reconstructed images
-for i in range(10):
-    ax = plt.subplot(1, 10, i + 1)
-    plt.imshow(decoded_imgs[i])
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
