fcdfce8fb828ff534dbc38da45a120643715d0f6
DimimiS <dimiskama115@gmail.com>
2024-05-05 10:53:05 +0100

    fixed image to array issue

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from keras.preprocessing import image
+import matplotlib.pyplot as plt
 
 
 # import the dataset from local directory
@@ -26,12 +27,11 @@ x_train = np.reshape(x_train, (len(x_train), 32, 32, 3))
 x_test = np.reshape(x_test, (len(x_test), 32, 32, 3))
 
 # these are all the unlabeled data
-stl_train_path = 'C:\\Users\\dimis\\Desktop\\image-compression\\img\\'
+stl_train_path = 'C:\\Users\\dimis\\Desktop\\image-compression\\stl_train\\'
 
 stl_train = []
 for filename in os.listdir(stl_train_path):
-    img = image.load_img(stl_train_path+filename, target_size=(96, 96, 3))  # Add the full path to the image file
-    stl_train.append(image.img_to_array(img))
+    stl_train.append(plt.imread(stl_train_path+filename))
     print(filename)
 stl_train = np.array(stl_train)
 
@@ -40,18 +40,9 @@ stl_test_path = 'C:\\Users\\dimis\\Desktop\\image-compression\\stl_test\\'
 
 stl_test = []
 for filename in os.listdir(stl_test_path):
-    img = image.load_img(stl_test_path+filename, target_size=(96, 96, 3))  # Add the full path to the image file
-    stl_test.append(image.img_to_array(img))
+    stl_test.append(plt.imread(stl_test_path+filename))
     print(filename)
 stl_test = np.array(stl_test)
 
-
-# Normalize the images
-slt_train = x_train.astype('float32') / 255
-slt_test = x_test.astype('float32') / 255
-
-# reshape data
-stl_train = np.reshape(stl_train, (len(stl_train), 96, 96, 3))
-stl_test = np.reshape(stl_test, (len(stl_test), 96, 96, 3))
 print(stl_train.shape)
 print(stl_test.shape)
