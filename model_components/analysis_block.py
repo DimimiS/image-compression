95435b749a00f582310bf5967518fc5ddb35df33
DimimiS <dimiskama115@gmail.com>
2024-07-13 22:22:27 +0300

    Train and  save model for just cnn implementation

--- model_components/analysis_block.py
+++ model_components/analysis_block.py
@@ -1,16 +1,17 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from model_components.GDN import GDN
+from .GDN import GDN
 
 # Analysis Block: This block encodes the input image into a latent space representation
 def analysis_block(input_shape):
     inputs = tf.keras.Input(shape=input_shape)
-    x = layers.Conv2D(128, kernel_size=5, strides=2, padding='same')(inputs)
-    x = GDN()(x)
-    x = layers.Conv2D(128, kernel_size=5, strides=2, padding='same')(x)
-    x = GDN()(x)
-    x = layers.Conv2D(128, kernel_size=5, strides=2, padding='same')(x)
-    x = GDN()(x)
-    x = layers.Conv2D(128, kernel_size=5, strides=2, padding='same')(x)
-    x = GDN()(x)
+    x = layers.Conv2D(3, (3, 3), activation='relu', padding='same')(inputs)
+    # x = GDN()(x)
+    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    # x = GDN()(x)
+    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    # x = GDN()(x)
+    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    # x = GDN()(x)
+    print(x.shape)
     return models.Model(inputs, x, name='analysis_block')
--- model_components/synthesis_block.py
+++ model_components/synthesis_block.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from model_components.GDN import GDN
+from .GDN import GDN
 
 # Synthesis Block: This block decodes the latent space representation back into the image space
 def synthesis_block(output_shape):
     inputs = tf.keras.Input(shape=output_shape)
-    x = layers.Conv2DTranspose(128, kernel_size=5, strides=2, padding='same')(inputs)
-    x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(128, kernel_size=5, strides=2, padding='same')(x)
-    x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(128, kernel_size=5, strides=2, padding='same')(x)
-    x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(3, kernel_size=5, strides=2, padding='same')(x)
+    print(inputs.shape)
+    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(inputs)
+    # x = GDN(inverse=True)(x)
+    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(x)
+    # x = GDN(inverse=True)(x)
+    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(x)
+    # x = GDN(inverse=True)(x)
+    x = layers.Conv2DTranspose(3, (3, 3), activation='relu', padding='same')(x)
     return models.Model(inputs, x, name='synthesis_block')
