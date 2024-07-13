f610920e859275f57c1038a5b9cdedaeb4769481
DimimiS <dimiskama115@gmail.com>
2024-07-13 17:08:16 +0300

    Load imagenette 320 images

--- model_components/dataset.py
+++ model_components/dataset.py
@@ -4,28 +4,37 @@ import os
 
 
 # Download the Imagenette Dataset
-path = untar_data(URLs.IMAGENETTE)
+path = untar_data(URLs.IMAGENETTE_320)
 train_dir = os.path.join(path, 'train')
 valid_dir = os.path.join(path, 'val')
 
 # Prepare the Data Loaders using ImageDataGenerator
-train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-valid_datagen = ImageDataGenerator(rescale=1./255)
-
-# Training data generator
+train_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size=(256, 256),
+    target_size=(320, 320),
     batch_size=32,
-    class_mode='input',  # Set to 'input' for image-to-image autoencoding
-    subset='training'
+    class_mode='input'
 )
 
-# Validation data generator
-validation_generator = train_datagen.flow_from_directory(
+valid_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = valid_datagen.flow_from_directory(
     valid_dir,
-    target_size=(256, 256),
+    target_size=(320, 320),
     batch_size=32,
-    class_mode='input',  # Set to 'input' for image-to-image autoencoding
-    subset='validation'
+    class_mode='input'
 )
+
+# #  Display images
+# import matplotlib.pyplot as plt
+
+# # Display a few images from the training set
+# images, _ = next(train_generator)
+# plt.figure(figsize=(10, 10))
+# for i in range(9):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(images[i])
+#     plt.axis("off")
+
+# plt.show()
+
