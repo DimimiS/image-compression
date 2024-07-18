5bfa1016b303ae21f911ae5fc01683382f9800fe
DimimiS <dimiskama115@gmail.com>
2024-07-19 00:21:05 +0300

    Set GDN to original from tfc

--- model_components/GDN.py
+++ model_components/GDN.py
@@ -1,36 +1,13 @@
-import tensorflow as tf
+import tensorflow_compression as tfc
+from tensorflow.keras import layers
+import os
 
-class GDN(tf.keras.layers.Layer):
-    def __init__(self, channels, gamma_init=0.1, inverse=False, **kwargs):
+# Generalized Divisive Normalization (GDN) Layer
+class GDN(layers.Layer):
+    def __init__(self, inverse=False, **kwargs):
         super(GDN, self).__init__(**kwargs)
-        self.channels = channels
-        self.gamma_init = gamma_init
         self.inverse = inverse
-
-    def build(self, input_shape):
-        if self.inverse:
-            # Initialize weights for inverse GDN if needed
-            self.gamma = self.add_weight(
-                name='gamma_inverse',
-                shape=(1, 1, self.channels, self.channels),
-                initializer=tf.keras.initializers.RandomNormal(mean=self.gamma_init, stddev=0.05),
-                trainable=True
-            )
-        else:
-            # Initialize weights for original GDN
-            self.gamma = self.add_weight(
-                name='gamma',
-                shape=(1, 1, self.channels, self.channels),
-                initializer=tf.keras.initializers.RandomNormal(mean=self.gamma_init, stddev=0.05),
-                trainable=True
-            )
-        super(GDN, self).build(input_shape)
-
+        self.gdn = tfc.GDN(inverse=inverse)
+    
     def call(self, inputs):
-        if self.inverse:
-            # Implement the inverse GDN operation
-            x = tf.nn.relu(tf.nn.conv2d(inputs, tf.linalg.inv(self.gamma), strides=[1, 1, 1, 1], padding='SAME'))
-        else:
-            # Original GDN operation
-            x = tf.nn.relu(tf.nn.conv2d(inputs, self.gamma, strides=[1, 1, 1, 1], padding='SAME'))
-        return x
+        return self.gdn(inputs)
