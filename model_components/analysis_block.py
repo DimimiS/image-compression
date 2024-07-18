07a1013850e84111098b8513e6ae8a71bc5238e8
DimimiS <dimiskama115@gmail.com>
2024-07-18 19:39:48 +0300

    Leaky relu activation

--- model_components/analysis_block.py
+++ model_components/analysis_block.py
@@ -5,13 +5,16 @@ from .GDN import GDN
 # Analysis Block: This block encodes the input image into a latent space representation
 def analysis_block(input_shape):
     inputs = tf.keras.Input(shape=input_shape)
-    x = layers.Conv2D(3, (3, 3), activation='relu', padding='same')(inputs)
-    # x = GDN()(x)
-    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-    # x = GDN()(x)
-    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-    # x = GDN()(x)
-    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = layers.Conv2D(3, (3, 3), padding='same')(inputs)
+    x = layers.LeakyReLU()(x)
     x = GDN()(x)
+
+    for _ in range(2):  # Simplified loop for repetitive blocks
+        x = layers.Conv2D(128, (3, 3), padding='same')(x)
+        x = layers.LeakyReLU()(x)
+        x = GDN()(x)  # Ensure channels is an integer
+
+    x = layers.Conv2D(128, (3, 3), padding='same')(x)
+    x = layers.LeakyReLU()(x)
     print(x.shape)
     return models.Model(inputs, x, name='analysis_block')
--- model_components/synthesis_block.py
+++ model_components/synthesis_block.py
@@ -4,13 +4,18 @@ from .GDN import GDN
 
 # Synthesis Block: This block decodes the latent space representation back into the image space
 def synthesis_block(output_shape):
+    
     inputs = tf.keras.Input(shape=output_shape)
     print(inputs.shape)
-    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(inputs)
-    x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(x)
-    # x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(128, (3, 3), activation='relu', padding='same')(x)
-    # x = GDN(inverse=True)(x)
-    x = layers.Conv2DTranspose(3, (3, 3), activation='relu', padding='same')(x)
+    x = layers.Conv2DTranspose(128, (3, 3), padding='same')(inputs)
+    x = layers.LeakyReLU()(x)
+    x = GDN( inverse=True)(x)
+
+    for _ in range(2):  # Simplified loop for repetitive blocks
+        x = layers.Conv2DTranspose(128, (3, 3), padding='same')(x)
+        x = layers.LeakyReLU()(x)
+        x = GDN( inverse=True)(x)  # Ensure channels is an integer
+
+    x = layers.Conv2DTranspose(3, (3, 3), padding='same')(x)
+    x = layers.LeakyReLU()(x)
     return models.Model(inputs, x, name='synthesis_block')
