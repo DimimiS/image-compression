0e71a6cbc08241ee2d38f71acfef48328a98e787
DimimiS <dimiskama115@gmail.com>
2024-07-20 19:27:09 +0300

    Calculate bpp for images

--- display_results.py
+++ display_results.py
@@ -4,6 +4,9 @@ from model_components.dataset import train_generator, validation_generator
 import numpy as np
 import cv2
 from model_components.model_metrics import psnr, ms_ssim
+from model_components.custom_loss import RateDistortionLoss
+
+# loss = RateDistortionLoss()
 
 # Load the model
 model = keras.models.load_model('/home/dimitra/Desktop/image-compression/cnn-gdn.keras', custom_objects={'psnr': psnr, 'ms_ssim': ms_ssim})
@@ -20,6 +23,27 @@ def yuv_to_rgb(yuv_image):
     # rgb_image = np.clip(rgb_image, 0, 255)  # Assuming the image is in 8-bit format
     return rgb_image
 
+
+def calculate_bit_rate(image_array):
+    # Get image dimensions and number of channels
+    height, width, num_channels = image_array.shape
+    
+    # For most images, each channel will be 8 bits (1 byte)
+    bits_per_channel = 8
+    
+    # Calculate the bit rate
+    total_pixels = width * height
+    bit_rate = total_pixels * num_channels * bits_per_channel
+    
+    return bit_rate
+
+original_bit_rate = calculate_bit_rate(train[0][0])
+compressed_bit_rate = calculate_bit_rate(compressed_train[0])
+
+# Get image's bit rate per pixel
+original_bit_rate_per_pixel = original_bit_rate / (train[0][0].shape[0] * train[0][0].shape[1])
+compressed_bit_rate_per_pixel = compressed_bit_rate / (compressed_train[0].shape[0] * compressed_train[0].shape[1])
+
 #  Display images RGB ranges
 print(f"Training Image RGB Range: {yuv_to_rgb(compressed_train[0]).min(), yuv_to_rgb(compressed_train[0]).max()}")
 
@@ -29,13 +53,13 @@ plt.subplot(2, 2, 1)
 plt.title('Original Image')
 plt.imshow((train[0][0]))
 plt.subplot(2, 2, 3)
-plt.title('Original Image')
+plt.title('Original Image | Bit Rate: {:.2f} bits'.format(original_bit_rate_per_pixel))
 plt.imshow(yuv_to_rgb(train[0][0]))
 plt.subplot(2, 2, 2)
 plt.title('Compressed Image')
 plt.imshow((compressed_train[0]))
 plt.subplot(2, 2, 4)
-plt.title('Compressed Image')
+plt.title('Compressed Image | Bit Rate: {:.2f} bits'.format(compressed_bit_rate_per_pixel))
 plt.imshow(yuv_to_rgb(compressed_train[0]))
 
 plt.show()
