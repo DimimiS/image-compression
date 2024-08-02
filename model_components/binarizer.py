7ed535910d68971e13db418b13feb8d8f394f96b
DimimiS <dimiskama115@gmail.com>
2024-08-02 20:24:34 +0300

    Binarizer implementation

--- model_components/binarizer.py
+++ model_components/binarizer.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-# Binarizer: This layer quantizes the encoded latent space representation to binary values
-class Binarizer(layers.Layer):
-    def __init__(self, **kwargs):
-        super(Binarizer, self).__init__(**kwargs)
-    
+class Sign(layers.Layer):
     def call(self, inputs):
         return tf.sign(inputs)
+
+class Binarizer(layers.Layer):
+    def __init__(self):
+        super(Binarizer, self).__init__()
+        self.conv = layers.Conv2D(128, kernel_size=1, use_bias=False)
+        self.sign = Sign()
+
+    def call(self, inputs):
+        feat = self.conv(inputs)
+        x = tf.tanh(feat)
+        return self.sign(x)
--- model_components/model.py
+++ model_components/model.py
@@ -7,27 +7,17 @@ from .rnn_block import RNNBlock
 from .synthesis_block import synthesis_block
 
 # Image Compression Model: This is the main model that integrates all the components
-
 class ImageCompressionModel(tf.keras.Model):
     def __init__(self, input_shape, rnn_units=128, **kwargs):
         super(ImageCompressionModel, self).__init__(**kwargs)
         self.analysis_block = analysis_block(input_shape)
         self.binarizer = Binarizer()
         self.rnn_block_enc = RNNBlock(rnn_units)
-        # Add a reshape layer here if the rnn_block_enc doesn't output the expected shape
-        self.reshape_before_dim_reduction = layers.Reshape((32, 256, 128))  # Adjust the target shape as needed
-        self.dimension_reduction_layer = layers.Conv2D(128, (1, 1), strides=(8, 1), padding='same')
-        self.reshape_layer = Reshape((32, 32, 128))
         self.synthesis_block = synthesis_block(self.analysis_block.output_shape[1:])
         self.rnn_block_dec = RNNBlock(rnn_units)
 
     def call(self, inputs):
-        x = self.analysis_block(inputs)
-        # x = self.binarizer(x)
-        # x = self.rnn_block_enc(x)
-        # # Apply the reshape before dimension reduction if needed
-        # x = self.reshape_before_dim_reduction(x)
-        # x = self.dimension_reduction_layer(x)
-        # x = self.reshape_layer(x)
-        x = self.synthesis_block(x)
-        return x
+        encoded = self.analysis_block(inputs)
+        x = self.binarizer(encoded)
+        decoded = self.synthesis_block(x)
+        return encoded, decoded
