b34c77611f1b0b0e41ef57c10808a2fecc3724ea
DimimiS <dimiskama115@gmail.com>
2024-08-04 20:36:43 +0300

    Improve binarizer

--- model_components/binarizer.py
+++ model_components/binarizer.py
@@ -6,12 +6,23 @@ class Sign(layers.Layer):
         return tf.sign(inputs)
 
 class Binarizer(layers.Layer):
-    def __init__(self):
+    def __init__(self, num_channels=128):
         super(Binarizer, self).__init__()
-        self.conv = layers.Conv2D(128, kernel_size=1, use_bias=False)
+        self.conv = layers.Conv2D(
+            num_channels,
+            kernel_size=1,
+            use_bias=False,
+            kernel_initializer='he_normal'  # Using He normal initialization
+        )
+        self.batch_norm = layers.BatchNormalization()  # Adding Batch Normalization
         self.sign = Sign()
 
     def call(self, inputs):
         feat = self.conv(inputs)
+        feat = self.batch_norm(feat)  # Normalize the feature maps
         x = tf.tanh(feat)
-        return self.sign(x)
+
+        # Binarization with Straight-Through Estimator
+        binary_output = self.sign(x)
+        # Using straight-through estimator for gradients
+        return tf.where(tf.abs(x) > 0.5, tf.ones_like(x), tf.zeros_like(x)) + (inputs - tf.where(tf.abs(x) > 0.5, tf.ones_like(x), tf.zeros_like(x)))
