909dd06b83e488c3f85ce508868ee1e9377467c0
DimimiS <dimiskama115@gmail.com>
2024-08-06 01:06:10 +0300

    More fail safe code, implement proper sign

--- model_components/binarizer.py
+++ model_components/binarizer.py
@@ -2,27 +2,29 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 class Sign(layers.Layer):
+    def __init__(self):
+        super(Sign, self).__init__()
+
     def call(self, inputs):
-        return tf.sign(inputs)
+        try:
+            prob = tf.random.uniform(tf.shape(inputs))
+            x = tf.where((1 - inputs) / 2 <= prob, tf.ones_like(inputs), -tf.ones_like(inputs))
+            return x
+        except:
+            return self.sign(inputs)
 
 class Binarizer(layers.Layer):
     def __init__(self, num_channels=128):
         super(Binarizer, self).__init__()
         self.conv = layers.Conv2D(
-            num_channels,
-            kernel_size=1,
+            filters=num_channels,
+            kernel_size=(1, 1),
             use_bias=False,
-            kernel_initializer='he_normal'  # Using He normal initialization
+            kernel_initializer='he_normal'
         )
-        self.batch_norm = layers.BatchNormalization()  # Adding Batch Normalization
         self.sign = Sign()
 
     def call(self, inputs):
         feat = self.conv(inputs)
-        feat = self.batch_norm(feat)  # Normalize the feature maps
         x = tf.tanh(feat)
-
-        # Binarization with Straight-Through Estimator
-        binary_output = self.sign(x)
-        # Using straight-through estimator for gradients
-        return tf.where(tf.abs(x) > 0.5, tf.ones_like(x), tf.zeros_like(x)) + (inputs - tf.where(tf.abs(x) > 0.5, tf.ones_like(x), tf.zeros_like(x)))
+        return self.sign(x)
