6b87f1371d54d6da352518595527f47f5304e887
DimimiS <dimiskama115@gmail.com>
2024-08-24 14:40:30 +0300

    post-meeting testing

--- model_components/custom_loss.py
+++ model_components/custom_loss.py
@@ -6,24 +6,30 @@ def compute_entropy(x):
     histogram = tf.histogram_fixed_width(x_flat, [tf.reduce_min(x_flat), tf.reduce_max(x_flat)], nbins=256)
     probabilities = histogram / tf.reduce_sum(histogram)
     probabilities = tf.clip_by_value(probabilities, 1e-10, 1.0)  # Avoid log(0)
-    entropy = -tf.reduce_sum(probabilities * tf.math.log(probabilities))
+    entropy = -tf.cast(tf.reduce_sum(probabilities * tf.math.log(probabilities)), tf.float32)/tf.math.log(2.0)
     return entropy
 
 class RateDistortionLoss(tf.keras.losses.Loss):
-    def __init__(self, lambda_param=0.001, **kwargs):
+    def __init__(self, lambda_param=0.01, **kwargs):
         super().__init__(**kwargs)
         self.lambda_param = lambda_param
         self.mse = tf.keras.losses.MeanSquaredError()
 
 
     def call(self, y_true, y_pred):
+        # Calculate the number of pixels in the image
+        num_pixels = tf.cast(tf.reduce_prod(tf.shape(y_true)[:-1]), tf.float32)
+
+        # Calculate compression ratio of a single image of the batch
+        bpp = compute_entropy(y_pred) / num_pixels
+
         # Distortion (D): Mean Squared Error
         distortion = self.mse(y_true, y_pred)
         
-        # Rate (R): Calculate based on the number of bits
-        rate = compute_entropy(y_pred)  # Using computed entropy directly
+        # Rate (R): Entropy of the compressed image
+        rate = bpp
 
-        distortion = tf.cast(distortion, tf.float32)
+        # Ensure rate is of type float32
         rate = tf.cast(rate, tf.float32)
         
         # Combine the losses
--- model_components/synthesis_block.py
+++ model_components/synthesis_block.py
@@ -7,13 +7,14 @@ def synthesis_block(output_shape):
     
     inputs = tf.keras.Input(shape=output_shape)
     x = layers.Conv2DTranspose(128, (3, 3), padding='same')(inputs)
+    x = layers.UpSampling2D((2, 2))(x)  # Upsample
     x = GDN(inverse=True)(x)
-    x = layers.LeakyReLU()(x)
-    # x = layers.UpSampling2D((2, 2))(x)  # Upsample
+    # x = layers.LeakyReLU()(x)
 
     for _ in range(2):  # Simplified loop for repetitive blocks
         x = layers.Conv2DTranspose(64, (3, 3), padding='same')(x)
         # x = GDN(inverse=True)(x)
+        x = layers.UpSampling2D((2, 2))(x)  # Upsample
         x = layers.LeakyReLU()(x)
 
     x = layers.Conv2DTranspose(3, (3, 3), padding='same')(x)
